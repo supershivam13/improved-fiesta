a0a7705837a97670e7b1792f678740795891c9ef
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-07-20 21:55:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -474,8 +474,17 @@ else:
                 # selected by the user and all computations are based on travel time
                 route = nx.shortest_path(G, orig, dest, 'travel_time',method=algo_value)
 
+                dict = {}
+                node_points=list(route)
+                node_waiting_times= np.random.permutation( np.arrange(len(node_points)))
+
+                time_charge=dict(zip(node_points,node_waiting_times))
+                time_charge
                 for i, val in enumerate(route):
+                    dict = {}
                     i, val, G.nodes[val]['x'], G.nodes[val]['y']
+
+                
 
 
             # if some error occurs during the generation of maps, then this except block will execute 
