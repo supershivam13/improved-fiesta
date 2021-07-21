58f9b5ccb1b17e6cd4aaaaa40543bcbc3375cb3d
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-07-22 00:32:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -483,7 +483,7 @@ else:
                 st.write("HYE3")
                 #route = nx.shortest_path(G, orig, dest, 'travel_time',method=algo_value)
                 for r in routes: 
-                    type(r)
+                    type(r) st.write("hi")
 
                 node_points=[]
                 #for  r in routes:
